44783861cf2e81774f9427c77f81fcb5f35740e3
sreejesh46 <86306486+sreejesh46@users.noreply.github.com>
2023-09-13 16:49:59 +0530

    PRINT NO % 2 == 0

---
+++ for_.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int limit=100;
+    printf
+    for(int i=10; i<=limit; i++){
+    if(i%2==0){
+        printf("%d ",i);
+                }
+                else{ printf"not a valid number");
+    }
+return  0;
+}
