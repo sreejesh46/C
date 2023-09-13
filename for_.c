b326ffa8ecd83b585c6f60216cf55943f6266eed
sreejesh46 <86306486+sreejesh46@users.noreply.github.com>
2023-09-13 16:55:05 +0530

    NO / 2 GIVES REM 0 LIMIT= 100

--- for_.c
+++ for_.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    int limit=100;
-    printf
-    for(int i=10; i<=limit; i++){
+    int limit =100;
+    for(int i=0; i<=limit; i++){
     if(i%2==0){
         printf("%d ",i);
                 }
-                else{ printf"not a valid number");
     }
 return  0;
 }
